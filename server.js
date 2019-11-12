c50f3ff5d55a7becbdb4342e43b621afac87950d
Daniel Gonzalez Garcia <daniel.gonzalez@danfoss.com>
2019-11-12 16:01:10 +0100

    done "parse arguments"

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log("Hello CLI")
+const { name, version, description } = require("./package.json")
+
+
+if (process.argv.length == 2) {
+	const helpMessage =
+`\nPackage ${description}
+Package ${version}
+\nUsage:\n
+--help\t\tHelp documentation
+--version\tInstalled package version
+`
+	console.log(helpMessage)
+} else if (process.argv.includes("--version")) {
+	const versionMessage = `${name} ${version}`
+	console.log(versionMessage)
+}
